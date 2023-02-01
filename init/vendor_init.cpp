7c86438fccd2a29490acecb75bf7ff94ffd0693f
Astr4y4L <astrayal@gmail.com>
2023-01-31 20:30:40 -0600

    Update vendor_init.cpp
    should probably just delete this

--- init/vendor_init.cpp
+++ init/vendor_init.cpp
@@ -26,12 +26,3 @@ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-#include "vendor_init.h"
-
-/* init vendor override stubs */
-
-__attribute__ ((weak))
-void vendor_load_properties()
-{
-}
